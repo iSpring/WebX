16ca3cecadd747e592e77a9dfa32853263c41122
Qun Sun <qsun@esri.com>
2017-03-23 12:45:49 +0800

    update

--- tutorials/webworker/worker.js
+++ tutorials/webworker/worker.js
@@ -1,7 +1,17 @@
-﻿//在worker中不能访问window对象，self是一个DedicatedWorkerGlobalScope对象
+﻿//在worker中不能访问window对象，可以使用self，self是一个DedicatedWorkerGlobalScope对象
 self.addEventListener("message", function(evt){
-  console.log("WebWorker收到message: " + evt.data);
-  var result = evt.data.toUpperCase();
-  self.postMessage(result);
-}, false);
+  var result = null;
+  if(typeof evt.data === "string"){
+    console.log("WebWorker工作线程收到主线程的字符串message: " + evt.data);
+    result = evt.data.toUpperCase();
 
+  }else{
+    console.log("WebWorker工作线程收到主线程的对象message: " + JSON.stringify(evt.data));
+    evt.data.msg = evt.data.msg.toUpperCase();
+    result = evt.data;
+  }
+
+  //向self发送message
+    self.postMessage(result);
+
+}, false);
