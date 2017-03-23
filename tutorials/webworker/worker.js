b3d21381dd071a5a1d39e4d3b55497fea111f1bc
Qun Sun <qsun@esri.com>
2017-03-23 12:09:16 +0800

    update webworker

---
+++ tutorials/webworker/worker.js
@@ -0,0 +1,7 @@
+﻿//在worker中不能访问window对象，self是一个DedicatedWorkerGlobalScope对象
+self.addEventListener("message", function(evt){
+  console.log("WebWorker收到message: " + evt.data);
+  var result = evt.data.toUpperCase();
+  self.postMessage(result);
+}, false);
+
