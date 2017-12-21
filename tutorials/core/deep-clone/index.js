b963480bb0a919ed580fe9e58566d36c2a56ab9e
十环 <sunqun.sq@alibaba-inc.com>
2017-12-21 13:48:55 +0800

    add deep-clone

---
+++ tutorials/core/deep-clone/index.js
@@ -0,0 +1,17 @@
+﻿function objDeepCopy ( originObj ) {
+    var newObj = Object.prototype.toString.call( originObj ) === '[object Array]' ? [] : {};
+    if(typeof originObj !== 'object' || originObj === null) {
+        // 不满足条件就直接返回原值
+        return originObj;   
+    } else if (window.JSON) {
+        // 如果支持JSON的window全局方法，这种方式效率是最高的。
+        newObj = JSON.parse(JSON.stringify(originObj));   
+    } else {
+        for(var property in obj){
+            if(obj.hasOwnProperty(property)){
+                newObj[property] = (typeof originObj[property] === 'object' && originObj[i] != null) ? objDeepCopy(originObj[property]) : originObj[property];
+            }
+        }
+    }
+    return newObj;
+};
